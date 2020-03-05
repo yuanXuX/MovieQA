fa512f4d93b4d59052e6600d6973b29c4e3faf59
yuanXuX <ysxy11@126.com>
2020-03-05 17:51:42 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+import sys
+import pandas as pd
+from pandas import Series, DataFrame
+from preprocess_data import Question
+# 创建问题处理对象，这样模型就可以常驻内存
+que=Question()
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+enablePrint()
+result=que.question_process("周润发是谁")
+print(result)
